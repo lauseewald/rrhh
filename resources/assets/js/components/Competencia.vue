ab289c7dfbbef4b7440373886191b73612a953b2
lauseewald <lau.seewald@hotmail.com>
2020-12-09 22:39:56 -0300

    abm competencia

---
+++ resources/assets/js/components/Competencia.vue
@@ -0,0 +1,395 @@
+<template>
+            <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">Escritorio</a></li>
+            </ol>
+            <div class="container-fluid">
+                <!-- Ejemplo de tabla Listado -->
+                <div class="card">
+                    <div class="card-header">
+                        <i class="fa fa-align-justify"></i> Competencias
+                        <button type="button" @click="abrirModal('incidencia','registrar')" class="btn btn-secondary">
+                            <i class="icon-plus"></i>&nbsp;Nuevo
+                        </button>
+                    </div>
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <div class="col-md-6">
+                                <div class="input-group">
+                                    <select class="form-control col-md-3" v-model="criterio">
+                                    
+                                    <option value="nombre">Nombre</option>
+                                   
+                                    </select>
+                                    <input type="text" v-model="buscar" @keyup.enter="listarCompetencia(1,buscar,criterio)" class="form-control" placeholder="Texto a buscar">
+                                    <button type="submit" @click="listarCompetencia(1,buscar,criterio)" class="btn btn-primary"><i class="fa fa-search"></i> Buscar</button>
+                                </div>
+                            </div>
+                        </div>
+                        <table class="table table-bordered table-striped table-sm">
+                            <thead>
+                                <tr>
+                                    <th>Nombre</th>
+                                    
+                                    <th>Estado</th>
+                                    <th>Opciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="competencia in arrayCompetencia" :key="competencia.id">
+                                   
+                                    <td v-text="competencia.nombre"></td>
+                                    <td v-text="competencia.diasMinimo"></td>
+                                    <td v-text="competencia.diasMaximo"></td>
+                                 
+                                    <td>
+                                        <div v-if="competencia.condicion">
+                                            <span class="badge badge-success">Activo</span>
+                                        </div>
+                                        <div v-else>
+                                            <span class="badge badge-danger">Desactivado</span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <button type="button" @click="abrirModal('competencia','actualizar',competencia)" class="btn btn-warning btn-sm">
+                                          <i class="icon-pencil"></i>
+                                        </button> &nbsp;
+                                        <template v-if="competencia.condicion">
+                                            <button type="button" class="btn btn-danger btn-sm" @click="desactivarCompetencia(competencia.id)">
+                                                <i class="icon-trash"></i>
+                                            </button>
+                                        </template>
+                                        <template v-else>
+                                            <button type="button" class="btn btn-info btn-sm" @click="activarCompetencia(competencia.id)">
+                                                <i class="icon-check"></i>
+                                            </button>
+                                        </template>
+                                    </td>
+                                </tr>                                
+                            </tbody>
+                        </table>
+                        <nav>
+                            <ul class="pagination">
+                                <li class="page-item" v-if="pagination.current_page > 1">
+                                    <a class="page-link" href="#" @click.prevent="cambiarPagina(pagination.current_page - 1,buscar,criterio)">Ant</a>
+                                </li>
+                                <li class="page-item" v-for="page in pagesNumber" :key="page" :class="[page == isActived ? 'active' : '']">
+                                    <a class="page-link" href="#" @click.prevent="cambiarPagina(page,buscar,criterio)" v-text="page"></a>
+                                </li>
+                                <li class="page-item" v-if="pagination.current_page < pagination.last_page">
+                                    <a class="page-link" href="#" @click.prevent="cambiarPagina(pagination.current_page + 1,buscar,criterio)">Sig</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <!-- Fin ejemplo de tabla Listado -->
+            </div>
+            <!--Inicio del modal agregar/actualizar-->
+            <div class="modal fade" tabindex="-1" :class="{'mostrar' : modal}" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
+                <div class="modal-dialog modal-primary modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title" v-text="tituloModal"></h4>
+                            <button type="button" class="close" @click="cerrarModal()" aria-label="Close">
+                              <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form id="modal-form" action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+                                <div class="form-group row">
+                                    <label class="col-md-3 form-control-label" for="text-input">Nombre (*)</label>
+                                    <div class="col-md-9">
+                                        <input type="text" v-model="nombre" class="form-control" placeholder="Nombre de la competencia">
+                                        
+                                    </div>
+                                </div>
+                                
+                                <div v-show="errorCompetencia" class="form-group row div-error">
+                                    <div class="text-center text-error">
+                                        <div v-for="error in errorMostrarMsjCompetencia" :key="error" v-text="error">
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" @click="cerrarModal()">Cerrar</button>
+                            <button type="button" v-if="tipoAccion==1" class="btn btn-primary" @click="registrarCompetencia()">Guardar</button>
+                            <button type="button" v-if="tipoAccion==2" class="btn btn-primary" @click="actualizarCompetencia()">Actualizar</button>
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
+            <!--Fin del modal-->
+        </main>
+</template>
+
+<script>
+import toastr from 'toastr'; 
+    export default {
+        data (){
+            return {
+                competencia_id: 0,
+                nombre : '',
+                diasMinimo : '',
+                diasMaximo : '',
+                arrayCompetencia : [],
+                modal : 0,
+                tituloModal : '',
+                tipoAccion : 0,
+                errorCompetencia : 0,
+                errorMostrarMsjCompetencia : [],
+                pagination : {
+                    'total' : 0,
+                    'current_page' : 0,
+                    'per_page' : 0,
+                    'last_page' : 0,
+                    'from' : 0,
+                    'to' : 0,
+                },
+                offset : 3,
+                criterio : 'nombre',
+                buscar : ''
+            }
+        },
+        computed:{
+            isActived: function(){
+                return this.pagination.current_page;
+            },
+            //Calcula los elementos de la paginación
+            pagesNumber: function() {
+                if(!this.pagination.to) {
+                    return [];
+                }
+                
+                var from = this.pagination.current_page - this.offset; 
+                if(from < 1) {
+                    from = 1;
+                }
+
+                var to = from + (this.offset * 2); 
+                if(to >= this.pagination.last_page){
+                    to = this.pagination.last_page;
+                }  
+
+                var pagesArray = [];
+                while(from <= to) {
+                    pagesArray.push(from);
+                    from++;
+                }
+                return pagesArray;             
+
+            }
+        },
+        methods : {
+            listarCompetencia (page,buscar,criterio){
+                let me=this;
+                var url= '/competencia?page=' + page + '&buscar='+ buscar + '&criterio='+ criterio;
+                axios.get(url).then(function (response) {
+                    var respuesta= response.data;
+                    me.arrayCompetencia = respuesta.competencias.data;
+                    me.pagination= respuesta.pagination;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            },
+            cambiarPagina(page,buscar,criterio){
+                let me = this;
+                //Actualiza la página actual
+                me.pagination.current_page = page;
+                //Envia la petición para visualizar la data de esa página
+                me.listarCompetencia(page,buscar,criterio);
+            },
+            registrarCompetencia(){
+                let me = this;
+                if (this.validarCompetencia()){
+                    return;
+                }
+                axios.post('/competencia/registrar',{
+                    'nombre': this.nombre
+                    
+                }).then(function (response) {
+                    me.cerrarModal();
+                    me.listarCompetencia(1,'','nombre');
+                    toastr.success('Se ha registrado la competencia', 'Actualizado', {timeOut: 5000})
+                }).catch(function (error) {
+                    console.log(error);
+                    toastr.error('Ha ocurrido un error', 'Error', {timeOut: 5000})
+                });
+            },
+            actualizarCompetencia(){
+
+                console.log('prueba');
+               if (this.validarCompetencia()){
+                    return;
+                }
+                
+                let me = this;
+                axios.put('/competencia/actualizar',{
+                    'nombre': this.nombre,
+                    'id': this.competencia_id
+                }).then(function (response) {
+                    me.cerrarModal();
+                    me.listarCompetencia(1,'','nombre');
+                    toastr.success('Se ha actualizado con exito', 'Actualizado', {timeOut: 5000})
+                }).catch(function (error) {
+                    console.log(error);
+                    toastr.error('Ha ocurrido un error', 'Error', {timeOut: 5000})
+                }); 
+            },
+            desactivarCompetencia(id){
+               swal({
+                title: 'Esta seguro de desactivar esta competencia?',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Aceptar!',
+                cancelButtonText: 'Cancelar',
+                confirmButtonClass: 'btn btn-success',
+                cancelButtonClass: 'btn btn-danger',
+                buttonsStyling: false,
+                reverseButtons: true
+                }).then((result) => {
+                if (result.value) {
+                    let me = this;
+
+                    axios.put('/competencia/desactivar',{
+                        'id': id
+                    }).then(function (response) {
+                        me.listarCompetencia(1,'','nombre');
+                        swal(
+                        'Desactivado!',
+                        'La competencia se ha desactivado con éxito.',
+                        'success'
+                        )
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+                    
+                    
+                } else if (
+                    // Read more about handling dismissals
+                    result.dismiss === swal.DismissReason.cancel
+                ) {
+                    
+                }
+                }) 
+            },
+            activarCompetencia(id){
+               swal({
+                title: 'Esta seguro de activar esta competencia?',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Aceptar!',
+                cancelButtonText: 'Cancelar',
+                confirmButtonClass: 'btn btn-success',
+                cancelButtonClass: 'btn btn-danger',
+                buttonsStyling: false,
+                reverseButtons: true
+                }).then((result) => {
+                if (result.value) {
+                    let me = this;
+
+                    axios.put('/competencia/activar',{
+                        'id': id
+                    }).then(function (response) {
+                        me.listarCompetencia(1,'','nombre');
+                        swal(
+                        'Activado!',
+                        'La competencia se ha activado con éxito.',
+                        'success'
+                        )
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+                    
+                    
+                } else if (
+                    // Read more about handling dismissals
+                    result.dismiss === swal.DismissReason.cancel
+                ) {
+                    
+                }
+                }) 
+            },
+            
+            
+            validarCompetencia(){
+                this.errorCompetencia=0;
+                this.errorMostrarMsjCompetencia =[];
+
+                if (!this.nombre) this.errorMostrarMsjCompetencia.push("Debe ingresar el nombre de la competencia");
+               
+                if (this.errorMostrarMsjCompetencia.length) this.errorCompetencia = 1;
+                return this.errorCompetencia;
+            },
+            cerrarModal(){
+                this.modal=0;
+                this.tituloModal='';
+                this.nombre='';
+            },
+            
+            abrirModal(modelo, accion, data = []){
+                switch(modelo){
+                    case "competencia":
+                    {
+                        switch(accion){
+                            case 'registrar':
+                            {
+                                this.modal = 1;
+                                this.tituloModal = 'Registrar competencia';
+                                this.nombre= '';
+                               
+                                this.tipoAccion = 1;
+                                break;
+                            }
+                            case 'actualizar':
+                            {
+                                this.modal=1;
+                                this.tituloModal='Actualizar competencia';
+                                this.tipoAccion=2;
+                                this.competencia_id=data['id'];
+                                this.nombre = data['nombre'];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            
+        },
+        mounted() {
+            this.listarCompetencia(1,this.buscar,this.criterio);
+        }
+    }
+</script>
+<style>    
+    .modal-content{
+        width: 100% !important;
+        position: absolute !important;
+    }
+    .mostrar{
+        display: list-item !important;
+        opacity: 1 !important;
+        position: absolute !important;
+        background-color: #3c29297a !important;
+    }
+    .div-error{
+        display: flex;
+        justify-content: center;
+    }
+    .text-error{
+        color: red !important;
+        font-weight: bold;
+    }
+</style>
